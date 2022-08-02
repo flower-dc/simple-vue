9951b53797eb8c1a75af11518bec7f7d0a61ba27
flower-dc <sword.jinqy@gmail.com>
2022-08-02 20:59:54 +0800

    feat: reactive & effect type supplement

--- src/reactvity/effect.ts
+++ src/reactvity/effect.ts
@@ -2,9 +2,9 @@ class ReactiveEffect {
     public deps: Set<ReactiveEffect>[] = [];
     private clearable = true;
     constructor(
-            private _fn: () => void, 
-            public scheduler?: () => void,
-            private onStop?: () => void
+            private _fn: () => any, 
+            public scheduler?: () => any,
+            private onStop?: () => any
         ) {
     }
     run() {
@@ -13,14 +13,14 @@ class ReactiveEffect {
     }
     stop() {
         if(this.clearable) {
-            cleanupEffects(this);
+            cleanup(this);
             this.onStop && this.onStop();
             this.clearable = false;
         }
     }
 }
 
-const cleanupEffects = (effect: ReactiveEffect) => {
+const cleanup = (effect: ReactiveEffect) => {
     effect.deps.forEach(d => {
         d.delete(effect);
     })
@@ -28,15 +28,15 @@ const cleanupEffects = (effect: ReactiveEffect) => {
 
 let activeEffect: ReactiveEffect | null = null;
 
-const targetMap = new WeakMap();
+const targetMap:WeakMap<object, Map<string|symbol, Set<ReactiveEffect>>> = new WeakMap();
 
-export const track = (target, key) => {
+export const track = <T extends object>(target:T, key:string|symbol) => {
     let depsMap = targetMap.get(target);
     if(!depsMap) {
         depsMap = new Map();
         targetMap.set(target, depsMap);
     }
-    let dep:Set<ReactiveEffect> = depsMap.get(key);
+    let dep:Set<ReactiveEffect>|undefined = depsMap.get(key);
     if(!dep) {
         dep = new Set();
         depsMap.set(key, dep);
@@ -45,10 +45,10 @@ export const track = (target, key) => {
     activeEffect?.deps.push(dep);
 }
 
-export const trigger = (target, key) => {
+export const trigger = <T extends object>(target:T, key:string|symbol) => {
     const depsMap = targetMap.get(target);
     if(!depsMap) return;
-    const dep:Set<ReactiveEffect> = depsMap.get(key);
+    const dep:Set<ReactiveEffect>|undefined = depsMap.get(key);
     if(!dep || !dep.size) return;
     for(const effect of dep) {
         if(effect.scheduler) {
@@ -69,7 +69,7 @@ type effectOptions = {
     onStop?: () => void;
 }
 
-export const effect = (fn:() => void, options?:effectOptions) => {
+export const effect = (fn:() => any, options?:effectOptions) => {
     const effect = new ReactiveEffect(fn, options?.scheduler, options?.onStop);
     effect.run();
     const runner:EffectRunner = effect.run.bind(effect);
@@ -77,6 +77,6 @@ export const effect = (fn:() => void, options?:effectOptions) => {
     return runner;
 }
 
-export const stop = (runner) => {
-    runner.effect.stop();
+export const stop = (runner:EffectRunner) => {
+    runner?.effect!.stop();
 }
